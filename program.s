0353f4631c4fa67542233ebb353e814d05ac1c0d
Jarrett Bullion <jjrreett@gmail.com>
2025-07-09 23:34:51 -0700

    Switch from sh comments to semicol comments

--- program.s
+++ program.s
@@ -12,7 +12,7 @@
     subleq a, b, ?
 .endm
 
-######## b = b + a ########
+;;;;;;;; b = b + a ;;;;;;;;
 .macro add a b
     sub a, z
     sub z, b
@@ -20,58 +20,58 @@
 .endm
 
 
-######## b = a ########
+;;;;;;;; b = a ;;;;;;;;
 .macro cpy a b
     clr b
     add a, b
 .endm
 
 
-######## a = a - 1 ########
+;;;;;;;; a = a - 1 ;;;;;;;;
 .macro dec a
     sub p1, a
 .endm
 
-######## a = a + 1 ########
+;;;;;;;; a = a + 1 ;;;;;;;;
 .macro inc a
     sub m1, a
 .endm
 
-## Branches
-#
-# Branch instructions change control flow based on flag conditions.
-# 
-# Op Code | Instruction                 | Affected Flags
-# --------|-----------------------------|----------------
-# BCC     | Branch if Carry Clear       | -
-# BCS     | Branch if Carry Set         | -
-# BEQ     | Branch if Zero Set          | -
-# BMI     | Branch if Negative Set      | -
-# BNE     | Branch if Zero Clear        | -
-# BPL     | Branch if Negative Clear    | -
-# BVC     | Branch if Overflow Clear    | -
-# BVS     | Branch if Overflow Set      | -
+;; Branches
+;
+; Branch instructions change control flow based on flag conditions.
+; 
+; Op Code | Instruction                 | Affected Flags
+; --------|-----------------------------|----------------
+; BCC     | Branch if Carry Clear       | -
+; BCS     | Branch if Carry Set         | -
+; BEQ     | Branch if Zero Set          | -
+; BMI     | Branch if Negative Set      | -
+; BNE     | Branch if Zero Clear        | -
+; BPL     | Branch if Negative Clear    | -
+; BVC     | Branch if Overflow Clear    | -
+; BVS     | Branch if Overflow Set      | -
 
-######## if a <= 0: jmp b ########
+;;;;;;;; if a <= 0: jmp b ;;;;;;;;
 .macro bleq a b
     subleq z, a, b
 .endm
 
-######## if a > 0: jmp b ########
+;;;;;;;; if a > 0: jmp b ;;;;;;;;
 .macro bgt a b
-                            # not(a <= 0) -> jump
-    bleq a, return        # a <= 0, don't take the jump to b
+                            ; not(a <= 0) -> jump
+    bleq a, return        ; a <= 0, don't take the jump to b
     jmp b
 return:
 .endm
 
-######## if a == 0: jmp b ########
+;;;;;;;; if a == 0: jmp b ;;;;;;;;
 .macro beq a b
-                            # not(a > 0) and (a+1 > 0) -> jump
-    bgt a, return          # a > 0, do not jump
+                            ; not(a > 0) and (a+1 > 0) -> jump
+    bgt a, return          ; a > 0, do not jump
     inc a
-    bgt a, decjump         # a+1 > 0, jump
-    dec a                  # fall through to de-increment a
+    bgt a, decjump         ; a+1 > 0, jump
+    dec a                  ; fall through to de-increment a
     jmp return
 decjump:
     dec a
@@ -79,28 +79,28 @@ decjump:
 return:
 .endm
 
-######## if a >= 0: jmp b ########
+;;;;;;;; if a >= 0: jmp b ;;;;;;;;
 .macro bpl a b
-                            # (a > 0) or (a == 0) -> jump
-    bgt a, b              # a > 0, jump
-    beq a, b              # a == 0, jump
+                            ; (a > 0) or (a == 0) -> jump
+    bgt a, b              ; a > 0, jump
+    beq a, b              ; a == 0, jump
 .endm
 
-######## if a <  0: jmp b ########
+;;;;;;;; if a <  0: jmp b ;;;;;;;;
 .macro bmi a b
-                            # not(a == 0) and (a <= 0) -> jump
-    beq a, return         # a == 0: therefor not a < 0, return
-    bleq a, b             # a <= 0, but not 0 -> a < 0, take the jump
+                            ; not(a == 0) and (a <= 0) -> jump
+    beq a, return         ; a == 0: therefor not a < 0, return
+    bleq a, b             ; a <= 0, but not 0 -> a < 0, take the jump
     return:
 .endm
 
 
 
-######## b = b * a ########
+;;;;;;;; b = b * a ;;;;;;;;
 .macro mul a b
         inc a
     loop:
-        subleq p1, a, break           # decrement 'a' by 1, break if 0
+        subleq p1, a, break           ; decrement 'a' by 1, break if 0
         add b, tmp
         jmp loop
     break:
@@ -112,19 +112,19 @@ return:
     return:
 .endm
 
-######## a = a + a ########
+;;;;;;;; a = a + a ;;;;;;;;
 .macro dbl a
 
     add a, a
 .endm
 
-######## b = b << a ########
+;;;;;;;; b = b << a ;;;;;;;;
 .macro lsl a b
 
         cpy a, counter
         inc counter
     loop:
-        subleq p1, counter, return         # decrement counter by 1, return if 0
+        subleq p1, counter, return         ; decrement counter by 1, return if 0
         dbl b
         jmp loop
     .data counter: 0 .endd
@@ -132,12 +132,12 @@ return:
 .endm
 
 
-######## b = b >> a ########
+;;;;;;;; b = b >> a ;;;;;;;;
 .macro lsr a b
         add literal_16, count
         sub b, count
         inc count
-        subleq p1, count, end           # if count is <= 1: end
+        subleq p1, count, end           ; if count is <= 1: end
         jmp rshift_start
     shift:
         dbl a
@@ -147,14 +147,14 @@ return:
     rshift_start:
         clr tmp
         add a, tmp
-        subleq m1, tmp, inc_out          # if the first bit of a is 1, inc out else shift
+        subleq m1, tmp, inc_out          ; if the first bit of a is 1, inc out else shift
         subleq tmp, tmp, check_break
 
     inc_out:
         inc out
 
     check_break:
-        subleq p1, count, end           # if count is <= 1: end
+        subleq p1, count, end           ; if count is <= 1: end
         jmp shift
 
     end:
@@ -171,18 +171,18 @@ return:
     return:
 .endm
 
-####### b = b * a ######### WIP
+;;;;;;; b = b * a ;;;;;;;;; WIP
 .macro fmul a b
     while:
-        bleq b, return           # if b <= 0: return
+        bleq b, return           ; if b <= 0: return
         cpy b, tmp
         lsl literal_15, tmp
-        bpl tmp, shift          # if b & 1:
-        add a, result           #     result += a
+        bpl tmp, shift          ; if b & 1:
+        add a, result           ;     result += a
         sub result, IO            
     shift:
-        dbl a           # double a
-        lsr p1, b   # halve b
+        dbl a           ; double a
+        lsr p1, b   ; halve b
         jmp while
 
     .data result: 0 tmp: 0 .endd
@@ -195,10 +195,10 @@ return:
 
 
 
-# dest is the place where we want the value
-# ptr is the addrs of the -value (as long as we negate the number going in, we can negate it going out)
-# executes the date block, falls through
-######## dest = *ptr ########
+; dest is the place where we want the value
+; ptr is the addrs of the -value (as long as we negate the number going in, we can negate it going out)
+; executes the date block, falls through
+;;;;;;;; dest = *ptr ;;;;;;;;
 .macro rpt ptr dest
     clr code_a
     clr code_a0
@@ -209,28 +209,28 @@ return:
     sub z, code_a1
     clr z
     cpy ptr, code_a
-    subleq dest, dest, code_a     # clear dest and jump code_a
+    subleq dest, dest, code_a     ; clear dest and jump code_a
 
     .data
-        # Two self modifying instructions
-        code_a: 0                # dest -= *code_a
+        ; Two self modifying instructions
+        code_a: 0                ; dest -= *code_a
                 dest
                 ?
-       code_a0: 0                # clr *code_a
+       code_a0: 0                ; clr *code_a
        code_a1: 0
                 ?
     .endd
 .endm
 
-######## *ptr = src ########
+;;;;;;;; *ptr = src ;;;;;;;;
 .macro wpt src ptr
     cpy ptr, code_b
     jmp code_a
 
     .data
-        code_a:  src              # This will become: subleq val, dest, ...
+        code_a:  src              ; This will become: subleq val, dest, ...
         code_b:  0
-        code_c:  ?                # next instruction
+        code_c:  ?                ; next instruction
     .endd
 .endm
 
@@ -251,26 +251,26 @@ return:
     .data return_addr: ? .endd
 .endm
 
-# This isn't exactly how return from subroutine typically works
-# the first pass pops the return addr off the stack
-# the second pass actually returns
+; This isn't exactly how return from subroutine typically works
+; the first pass pops the return addr off the stack
+; the second pass actually returns
 .macro rts
 
-        bleq second_pass, setup       # if second_pass <= 0 then jmp to setup 
-        clr second_pass               #     else clean up second_pass and ...
+        bleq second_pass, setup       ; if second_pass <= 0 then jmp to setup 
+        clr second_pass               ;     else clean up second_pass and ...
         .data
-            0                         # self modifying jmp to return_addr
+            0                         ; self modifying jmp to return_addr
             0
             return_addr: 0
 
-            second_pass: 0            # flag to branch on rerun
+            second_pass: 0            ; flag to branch on rerun
         .endd
     setup:
         inc second_pass
-        pop return_addr             # pop the return addr from the stack and put into self modifying jmp
+        pop return_addr             ; pop the return addr from the stack and put into self modifying jmp
 .endm
 
-######## 4 bit shift, inc output if overflow ########
+;;;;;;;; 4 bit shift, inc output if overflow ;;;;;;;;
 .macro byte_lshift_o input output
 
                            
@@ -296,7 +296,7 @@ return:
     no_overflow:
 .endm
 
-######## 8 bit shift, inc output if overflow ########
+;;;;;;;; 8 bit shift, inc output if overflow ;;;;;;;;
 .macro half_word_lshift_overflow input output
         dbl input
         cpy input, tmp
@@ -315,10 +315,10 @@ return:
     no_overflow:
 .endm
 
-######## 16 bit shift, inc output if overflow ########
+;;;;;;;; 16 bit shift, inc output if overflow ;;;;;;;;
 .macro lshift_overflow input output
         bpl input, no_overflow
-        inc output             # inc output if overflow, always shift input
+        inc output             ; inc output if overflow, always shift input
     no_overflow:
         dbl input
 .endm
@@ -331,22 +331,22 @@ return:
 
 .macro double_dabble_add_3 x
     cpy x, tmp
-    subleq literal_4, tmp, return      # if x ≤ 4, skip
-    add literal_3, x                  # else x += 3
+    subleq literal_4, tmp, return      ; if x ≤ 4, skip
+    add literal_3, x                  ; else x += 3
     jmp return
     .data tmp: 0 .endd
 return:
 .endm
 
-################################################
-#################### CODE ######################
-################################################
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;; CODE ;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 jmp test
 
-# ALIGNMENT
+; ALIGNMENT
 .data 
-    0     # IO
-    0     # INSPECT
+    0     ; IO
+    0     ; INSPECT
 .endd                    
 
 
